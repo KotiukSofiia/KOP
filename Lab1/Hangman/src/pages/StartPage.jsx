1c61aba3c7890f1b7d2a452071fe801498f7d45f
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-10-28 17:02:16 +0200

    Add dual animation logic (start page and game loss)

--- Lab1/Hangman/src/pages/StartPage.jsx
+++ Lab1/Hangman/src/pages/StartPage.jsx
@@ -5,8 +5,7 @@ const StartPage = ({ onStartGame }) => {
   return (
     <div className="start-page">
       <h1>HANGMAN</h1>      
-      <HangmanVisual mistakesCount={6} animate={true} /> 
-
+      <HangmanVisual mistakesCount={6} animateStartPage={true} />
       <p>Click to start!</p>
       <button onClick={onStartGame}>Start</button>
     </div>
