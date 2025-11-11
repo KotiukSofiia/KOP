c3cca330220f57aad51ac4165dd1db33264a6fac
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-11-11 15:30:44 +0200

    Add SettingsContext and provide it to the app

--- Lab1/Hangman/src/main.jsx
+++ Lab1/Hangman/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import App from './App.jsx';
+import { SettingsProvider } from './context/SettingsContext'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SettingsProvider> 
+      <App />
+    </SettingsProvider>
   </StrictMode>,
-)
+);
