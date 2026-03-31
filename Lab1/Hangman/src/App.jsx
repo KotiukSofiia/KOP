f7d067e4cffe8ca7ae4a1a4c96576d83db8db70b
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2026-03-31 20:04:12 +0300

    add concise GDPR Privacy Policy

--- Lab1/Hangman/src/App.jsx
+++ Lab1/Hangman/src/App.jsx
@@ -5,8 +5,19 @@ import GamePage from './pages/GamePage';
 import SettingsPage from './pages/SettingsPage'; 
 import styles from './App.module.css'; 
 import LeaderboardPage from './pages/LeaderboardPage';
+import CookieConsent from "react-cookie-consent";
 
 function App() {
+
+  const handleAcceptCookie = () => {
+    console.log("Cookie consent accepted! Initializing analytics...");
+    localStorage.setItem("gdpr_analytics_enabled", "true");
+  };
+
+  const handleDeclineCookie = () => {
+    console.log("Cookie consent declined! Tracking disabled.");
+    localStorage.setItem("gdpr_analytics_enabled", "false");
+  };
 
   return (
     <div className={styles.appContainer}>
@@ -21,6 +32,22 @@ function App() {
         
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+
+      <CookieConsent
+        location="bottom"
+        buttonText="Accept"
+        declineButtonText="Decline"
+        enableDeclineButton
+        onAccept={handleAcceptCookie}
+        onDecline={handleDeclineCookie}
+        cookieName="hangmanGdprConsent"
+        style={{ background: "#706c6c", alignItems: "center", zIndex: 9999 }}
+        buttonStyle={{ background: "#6ddf71", color: "white", fontSize: "14px", borderRadius: "5px", padding: "8px 15px", fontWeight: "bold" }}
+        declineButtonStyle={{ background: "#e56161", color: "white", fontSize: "14px", borderRadius: "5px", padding: "8px 15px", fontWeight: "bold" }}
+        expires={150}
+      >
+        This site uses cookies to provide the best user experience. By continuing to use the site, you agree to our privacy policy.{" "}
+      </CookieConsent>
     </div>
   );
 }
